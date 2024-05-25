ac7ab1f7c84ac6972e1d46505c0f3bbbb06f1967
HongVietBui <henrygphp@gmail.com>
2024-05-25 22:40:15 +0700

    Fixed Setting button not displayed issue

--- Assets/Script/Menu/SettingButtonScript.cs
+++ Assets/Script/Menu/SettingButtonScript.cs
@@ -10,6 +10,21 @@ public class SettingButtonScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //check if setting panel is null or not
+        if(settingPanel == null)
+        {
+            Debug.LogError("Setting Panel is null");
+            return;
+        }
+
+        //check if setting button is null or not
+        if(settingButton == null)
+        {
+            Debug.LogError("Setting Button is null");
+            return;
+        }
+
+
         settingPanel.SetActive(false);
         settingButton.onClick.AddListener(() =>
         {
