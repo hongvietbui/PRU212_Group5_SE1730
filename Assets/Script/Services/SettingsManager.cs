012a9b3d89ce9b4a28024fa7ef500726cebb5a8a
HongVietBui <henrygphp@gmail.com>
2024-05-28 02:20:12 +0700

    Added main menu
    Delete sample scene
    Added Singleton and game management classes

---
+++ Assets/Script/Services/SettingsManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsManager : MonoBehaviour
+{
+    public Slider volumeSlider;
+    public TextMeshProUGUI volumeValue;
+    public GameObject settingPanel;
+    public GameObject mainMenuPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        volumeSlider.onValueChanged.AddListener(SetVolume);
+        volumeSlider.value = GameManager.Instance.Volume;
+    }
+
+    //set volume
+    public void SetVolume(float volume)
+    {
+        GameManager.Instance.Volume = volume;
+    }
+
+    //show settings panel
+    public void ShowSettings()
+    {
+        settingPanel.SetActive(true);
+        mainMenuPanel.SetActive(false);
+    }
+
+    //hide settings panel
+    public void HideSettings()
+    {
+        settingPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Change volume text depends on slider value
+        volumeValue.text = (int)(volumeSlider.value * 100) + "%";
+    }
+}
