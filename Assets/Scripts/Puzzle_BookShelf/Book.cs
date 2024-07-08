71c623708bbf83fa0929f871a8e396c049dc2b00
ADMIN\Admin <minhvche161378@fpt.edu.vn>
2024-07-08 08:32:48 +0700

    Modify Gameplay Puzzle

---
+++ Assets/Scripts/Puzzle_BookShelf/Book.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Book : MonoBehaviour
+{
+    public int originalIndex;
+
+    private void Start()
+    {
+        // Sử dụng tên của GameObject làm tên sách
+        string bookName = gameObject.name;
+    }
+}
