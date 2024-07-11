66d4a3b1ee6ecf649273e8fbadd1ed7a0fe1b71d
ADMIN\Admin <minhvche161378@fpt.edu.vn>
2024-07-11 08:23:43 +0700

    update data

--- Assets/Scripts/Puzzle_BookShelf/BookshelfManager.cs
+++ Assets/Scripts/Puzzle_BookShelf/BookshelfManager.cs
@@ -1,19 +1,10 @@
 using UnityEngine;
-using UnityEngine.UI;
 using System.Collections.Generic;
-using TMPro;
-using UnityEngine.SceneManagement;
 
 public class BookshelfManager : MonoBehaviour
 {
     public List<GameObject> books;
     public Transform[] bookPositions;
-
-    public GameObject detailImagePanel;
-    public Image detailImageRenderer;
-    public Button closeButton;
-    public TextMeshProUGUI correctBooksText;
-    public GameObject congratulationsImage; // Add this for displaying congratulations image
 
     private GameObject selectedBook;
 
@@ -21,9 +12,6 @@ public class BookshelfManager : MonoBehaviour
     {
         ShuffleBooks();
         PlaceBooks();
-        detailImagePanel.SetActive(false);
-        congratulationsImage.SetActive(false);
-        UpdateCorrectBooksText();
     }
 
     private void Update()
@@ -31,10 +19,6 @@ public class BookshelfManager : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             SelectBook();
-        }
-        else if (Input.GetMouseButtonDown(1))
-        {
-            ShowDetailImage();
         }
     }
 
@@ -55,7 +39,8 @@ public class BookshelfManager : MonoBehaviour
         for (int i = 0; i < books.Count; i++)
         {
             Vector3 position = bookPositions[i].position;
-            position.z = 0;
+            position.z = 0; // Ensure books are placed at z = 0 in a 2D setup
+
             books[i].transform.position = position;
             books[i].GetComponent<Book>().originalIndex = i;
             Debug.Log("Book " + books[i].name + " placed at: " + books[i].transform.position);
@@ -88,80 +73,45 @@ public class BookshelfManager : MonoBehaviour
         }
     }
 
-    private void ShowDetailImage()
-    {
-        Vector2 raycastPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        RaycastHit2D hit = Physics2D.Raycast(raycastPosition, Vector2.zero);
-
-        if (hit.collider != null)
-        {
-            GameObject clickedBook = hit.collider.gameObject;
-
-            if (clickedBook != null && clickedBook.GetComponent<Book>() != null)
-            {
-                if (detailImagePanel != null && detailImageRenderer != null)
-                {
-                    detailImagePanel.SetActive(true);
-                    detailImageRenderer.sprite = clickedBook.GetComponent<Book>().detailImage;
-                    detailImageRenderer.transform.SetAsLastSibling();
-                }
-                else
-                {
-                    Debug.LogWarning("Detail Image Panel or Detail Image Renderer is not assigned in the inspector.");
-                }
-            }
-        }
-        else
-        {
-            Debug.LogWarning("No collider found under the mouse position.");
-        }
-    }
-
-    public void HideDetailImage()
-    {
-        detailImagePanel.SetActive(false);
-    }
-
     private void SwapBooks(GameObject book1, GameObject book2)
     {
         int index1 = book1.GetComponent<Book>().originalIndex;
         int index2 = book2.GetComponent<Book>().originalIndex;
 
+        // Save initial positions
         Vector3 tempPosition = book1.transform.position;
         int tempIndex = index1;
 
+        // Swap positions
         book1.transform.position = bookPositions[index2].position;
         book2.transform.position = bookPositions[index1].position;
 
+        // Update the books array
         books[index1] = book2;
         books[index2] = book1;
 
+        // Update original indices
         book1.GetComponent<Book>().originalIndex = index2;
         book2.GetComponent<Book>().originalIndex = tempIndex;
 
         Debug.Log("Swapped " + book1.name + " with " + book2.name);
 
+        // Debug to check positions and indices after swapping
         Debug.Log(book1.name + " is now at position: " + book1.transform.position + " with index: " + book1.GetComponent<Book>().originalIndex);
         Debug.Log(book2.name + " is now at position: " + book2.transform.position + " with index: " + book2.GetComponent<Book>().originalIndex);
-
-        UpdateCorrectBooksText();
     }
 
     private void CheckForCompletion()
     {
         string correctOrder = "fptuniversity";
-        bool isCorrect = true;
+        string currentOrder = "";
 
         for (int i = 0; i < books.Count; i++)
         {
-            if (books[i].name[0].ToString().ToLower() != correctOrder[i].ToString().ToLower())
-            {
-                isCorrect = false;
-                break;
-            }
+            currentOrder += books[i].name[0];
         }
 
-        if (isCorrect)
+        if (currentOrder == correctOrder)
         {
             DisplayCompletionMessage();
         }
@@ -169,31 +119,6 @@ public class BookshelfManager : MonoBehaviour
 
     private void DisplayCompletionMessage()
     {
-        congratulationsImage.SetActive(true); // Show congratulations image
-        // Optionally, transition to another screen
-       // SceneManager.LoadScene("AAA"); // Load scene "AAA"
-    }
-
-    private void UpdateCorrectBooksText()
-    {
-        string correctOrder = "fptuniversity";
-        int correctBooks = 0;
-
-        for (int i = 0; i < books.Count; i++)
-        {
-            if (books[i].name[0].ToString().ToLower() == correctOrder[i].ToString().ToLower())
-            {
-                correctBooks++;
-            }
-        }
-
-        if (correctBooksText != null)
-        {
-            correctBooksText.text = "Correct Books: " + correctBooks;
-        }
-        else
-        {
-            Debug.LogWarning("Correct Books Text is not assigned in the inspector.");
-        }
+        Debug.Log("You have solved the puzzle!");
     }
 }
