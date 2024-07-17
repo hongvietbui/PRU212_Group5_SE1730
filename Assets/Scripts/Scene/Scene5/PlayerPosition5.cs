fa5e9b1c3218544bd7cdcc3c19ae2825b88835d6
Phuc5438 <hoangphucoh@gmail.com>
2024-07-17 17:38:18 +0700

    update
    start dialogue, save position

--- Assets/Scripts/Scene/Scene5/PlayerPosition5.cs
+++ Assets/Scripts/Scene/Scene5/PlayerPosition5.cs
@@ -5,25 +5,8 @@ using UnityEngine;
 public class PlayerPosition5 : MonoBehaviour
 {
     public Transform playerTransform;
-    public GameObject completionObject; // Thêm biến này để giữ tham chiếu tới GameObject cần kích hoạt
-
-    void Start()
+	void Start()
     {
-        if (PlayerPrefs.GetInt("PuzzleSolved", 0) == 1)
-        {
-            if (completionObject != null)
-            {
-                completionObject.SetActive(true); // Kích hoạt GameObject khi puzzle đã giải quyết
-            }
-        }
-        else
-        {
-            if (completionObject != null)
-            {
-                completionObject.SetActive(false); // Đảm bảo GameObject này bị vô hiệu hóa nếu puzzle chưa được giải quyết
-            }
-        }
-
         playerTransform.position = PuzzleState.savedPosition;
     }
 }
